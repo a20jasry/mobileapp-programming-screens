9f526613ac6dcd100e93eddd0481b44c7a29a2bb
a20jasry <81618961+a20jasry@users.noreply.github.com>
2021-04-30 14:13:05 +0200

    Intent + starta activity

--- app/src/main/java/com/example/screens/LoginActivity.java
+++ app/src/main/java/com/example/screens/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG", "starta aktiviteten");
+
+                Intent intent = new Intent(LoginActivity. this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
